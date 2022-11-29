f86e3c082be65b7a082105016e084f25bd123cf2
owenprotz <owen.protz@stu.ocsb.ca>
2022-11-29 14:06:33 -0500

    finished nested (maybe?)

--- main/main.java
+++ main/main.java
@@ -1,5 +1,29 @@
-package main;
+import java.util.*;
+public class Main {
 
-public class main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		Random rnd=new Random();
+		System.out.println("Please put in your name");
+		String name = in.next();
+		System.out.println("Hi "+name+"!");
+		System.out.println("Please put in a number for an array size");
+		int size=in.nextInt();
+		double[] nums=new double[size];
+		nums= rnd;
+		for (int i=0;i<nums.length;i++) {
+//			nums=rnd.nextDouble(size);
+			System.out.print(nums);
+		}
+	
+		
+		for (double i = 0; i < nums.length; i++) {
+//			double[i]=
+			
+		}
+
+
+	}
 
 }
