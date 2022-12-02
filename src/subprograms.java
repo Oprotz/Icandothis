b348e032be277134611acf2c7d480a3ec71023f2
owenprotz <owen.protz@stu.ocsb.ca>
2022-12-02 14:23:15 -0500

    actually done now

--- src/subprograms.java
+++ src/subprograms.java
@@ -1,11 +1,47 @@
 
+import java.util.Scanner;
+
 public class subprograms {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-		
+		Scanner in = new Scanner(System.in);
+
+
+
+	System.out.println("What is the first string:");
+	String Uno = in.next();
+
+	
+
+	System.out.println("What is the second string:");
+		String Dos = in.next();
+	System.out.println(combine(Uno, Dos));
+
+	
+
+
+	}
+
+	
+
+	public static String combine(String Uno, String Dos) {
+
+		String Tres = "";
+
+	for (int i = 0; i < Math.max(Uno.length(), Dos.length()); i++) {
+
+			if (i < Uno.length()) {
+		Tres += Uno.charAt(i);
+
+			}
+
+		Tres += Dos.charAt(i);
+
+		}
+
+	
+
+return Tres;
 	}
 
 }
